516e441545bf0962760052d472d9a37af153aa92
Joanne Caroll <joannecaroll@UNIT001.(none)>
2014-09-22 16:28:42 +0800

    Weight on moon

--- src/Weight_On_Moon.java
+++ src/Weight_On_Moon.java
@@ -3,12 +3,12 @@ import java.util.*;
 public class Weight_On_Moon {
 	public static void main(String args[])
 	{
-		double weight_on_moon_kg, weight_on_earth_kg;
+		double weight_on_moon, weight_on_earth;
 		System.out.println("Enter weight on earth: ");
 		Scanner earthweight = new Scanner(System.in);
-		weight_on_earth_kg = earthweight.nextInt();
-		weight_on_moon_kg = weight_on_earth_kg * 0.17;
-		System.out.println("Your weight on moon is " + weight_on_moon_kg + ".");
+		weight_on_earth = earthweight.nextInt();  
+		weight_on_moon = weight_on_earth * 0.17;
+		System.out.println("Your weight on moon is " + weight_on_moon + ".");
 		
 	}
 }
